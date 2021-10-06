440d58c29a14bd9d986163634b191f8896c6e566
Rafael <rafaeljavaro750@gmail.com>
2021-10-06 12:08:15 -0300

    Adiciona Busca de Aceleração por ID da Aceleração e por ID da Empresa Parceira + Salva nova aceleração

---
+++ src/main/java/com/challenge/service/AccelerationService.java
@@ -0,0 +1,30 @@
+package com.challenge.service;
+
+import com.challenge.entity.Acceleration;
+import com.challenge.repository.AccelerationRepository;
+import com.challenge.service.interfaces.AccelerationServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+public class AccelerationService implements AccelerationServiceInterface {
+
+    @Autowired
+    AccelerationRepository repository;
+
+    @Override
+    public Optional<Acceleration> findById(Long id) {
+        return this.repository.findById(id);
+    }
+
+    @Override
+    public List<Acceleration> findByCompanyId(Long companyId) {
+        return this.repository.findByCompanyId(companyId);
+    }
+
+    @Override
+    public Acceleration save(Acceleration object) {
+        return this.repository.save(object);
+    }
+}
